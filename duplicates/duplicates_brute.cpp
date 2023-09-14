4923c5868ca5f0af47ee29c666ee087c45b6965e
sergey <revyakin8800@gmail.com>
2023-09-14 19:44:13 +0700

    did the second part of the lab

---
+++ duplicates/duplicates_brute.cpp
@@ -0,0 +1,23 @@
+#include "duplicates.h"
+#include <set>
+using namespace std;
+
+bool has_duplicates(const std::vector<int> &data) {
+    for (int i=0;i<data.size();i++){
+        for (int j=i;j<data.size();j++ ){
+            if (data[i]==data[j]){return true;}
+        }
+    }
+    return false;
+}
+
+std::vector<int> get_duplicates(const std::vector<int> &data) {
+    std::vector<int> result;
+    for (int i=0;i<data.size();i++){
+        for (int j=i;j<data.size();j++ ){
+            if (data[i]==data[j]){result.push_back(data[i]);}
+        }
+    }
+    
+    return result;
+}
---
+++ duplicates/duplicates_quick.cpp
@@ -0,0 +1,24 @@
+#include "duplicates.h"
+#include <set>
+using namespace std;
+
+bool has_duplicates(const std::vector<int> &data) {
+    set<int> my_set(data.begin(),data.end());
+    if (my_set.size()==data.size()){
+        return false;
+
+    }
+    return true;
+}
+
+std::vector<int> get_duplicates(const std::vector<int> &data) {
+    std::vector<int> result;
+    set<int> my_set;
+    for(const auto & elem: data){
+        if (my_set.find(elem)!=my_set.end()){//elem есть в set
+            result.push_back(elem);
+            }
+        my_set.insert(elem);
+    }
+    return result;
+}
